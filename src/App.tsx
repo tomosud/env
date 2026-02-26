87fe0bf8cb630f1884aae3d23f69c9fc5770ac95
tomosud <tomohisaoda@gmail.com>
2026-02-27 01:05:38 +0900

    Revert "add"
    This reverts commit 83e19aff584e737fc6bcd41a5b0ea2f62386b015.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { AppContent } from "./components/AppContent";
 import { AppLayout } from "./components/AppLayout";
 import { AppToolbar } from "./components/AppToolbar";
 import { CommandPalette } from "./components/CommandPalette";
-import { useUrlStateSync } from "./hooks/useUrlStateSync";
 
 export default function App() {
-  useUrlStateSync();
-
   return (
     <>
       <Toaster theme="dark" richColors position="bottom-center" />
